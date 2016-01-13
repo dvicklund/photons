42a86113800b1a4100b6e12167064d61ddc66590
David Vicklund <david@vicklund.com>
2016-01-13 01:32:58 -0800

    barest of bones setup

---
+++ gulpfile.js
@@ -0,0 +1,56 @@
+var gulp = require('gulp');
+var webpack = require('webpack-stream');
+var sass = require('gulp-sass');
+var minifyCss = require('gulp-cssnano');
+var concatCss = require('gulp-concat-css');
+var sourcemaps = require('gulp-sourcemaps');
+
+var paths = {
+  css: ['app/**/*.scss'],
+  html: ['app/**/*.html'],
+  js: ['app/**/*.js'],
+  test: ['test/testRoutes.js']
+};
+
+gulp.task('build:css', function() {
+  gulp.src('app/scss/application.scss')
+    .pipe(sourcemaps.init())
+    .pipe(sass())
+    .pipe(gulp.dest('app/css/'))
+    .pipe(concatCss('style.min.css'))
+    .pipe(minifyCss())
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest('build/'));
+});
+
+gulp.task('build:html', function() {
+  gulp.src('app/**/*.html')
+  .pipe(gulp.dest('build/'));
+});
+
+gulp.task('build:js', function() {
+  return gulp.src('app/js/entry.js')
+  .pipe(webpack({
+    output: {
+      filename: 'bundle.js'
+    }
+  }))
+  .pipe(gulp.dest('build/'));
+});
+
+gulp.task('watch:css', function() {
+	gulp.watch(paths.css, ['build:css']);
+});
+
+gulp.task('watch:html', function() {
+	gulp.watch(paths.html, ['build:html']);
+});
+
+gulp.task('watch:js', function() {
+	gulp.watch(paths.js, ['build:js']);
+});
+
+gulp.task('build:all', ['build:css', 'build:html', 'build:js']);
+gulp.task('test:all', ['test:mocha']);
+gulp.task('watch:all', ['watch:css', 'watch:html', 'watch:js']);
+gulp.task('default', ['build:all']);
---
+++ server.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var app = express();
+
+app.use(express.static(__dirname + '/build'));
+
+var port = process.env.PORT || 3000;
+
+app.listen(port, function() {
+	console.log('Server listening on port ' + (port || 3000));
+});
